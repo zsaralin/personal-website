f64368d3a104ac9ccaf7f0bdc423495a643d7e7f
Saralin <zsaralin@gmail.com>
2021-12-16 16:20:51 -0600

    Updates

---
+++ precache-manifest.973c920f1444762457c15c15cff9aa6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fc5e92b16c357c33a4a8c97995395a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd0175a38017119c30d",
+    "url": "/static/css/main.27fcf737.chunk.css"
+  },
+  {
+    "revision": "f05cfd33767ba537ef12",
+    "url": "/static/js/2.fd6087f4.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.fd6087f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd0175a38017119c30d",
+    "url": "/static/js/main.d221891e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "720eee3fd52a2aa62829983d21dc439d",
+    "url": "/static/media/SaralinZassman_Resume.720eee3f.pdf"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "c7c38155c0201c238d90c20899845dc8",
+    "url": "/static/media/purdy.c7c38155.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
